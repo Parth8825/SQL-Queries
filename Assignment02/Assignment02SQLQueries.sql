6e0ddfc3a64f206919f90084436f7f66c9dc1e14
Parth Darji <pdarji8825@conestogac.on.ca>
2022-08-18 12:47:10 -0400

    Aggregate queries done!

--- Assignment02/Assignment02SQLQueries.sql
+++ Assignment02/Assignment02SQLQueries.sql
@@ -1,3 +1,4 @@
+--JOINS
 -- Q14
 SELECT name AS salesman_name,
 cust_name, c.city 
@@ -57,3 +58,60 @@ WHERE (NOT ord_date = '2012-09-10' AND salesman_id <= 5005) OR purch_amt <= 1000
 SELECT * 
 FROM  orders 
 WHERE ord_date = '2012-08-17' OR (customer_id > 3005 AND purch_amt < 1000);
+
+--AGGREGATE
+-- Q24
+SELECT SUM(purch_amt) 
+FROM orders;
+
+-- Q25
+SELECT AVG(purch_amt) 
+FROM orders;
+
+-- Q26
+SELECT COUNT(DISTINCT salesman_id) 
+FROM customer;
+
+-- Q27
+SELECT COUNT(*) 
+FROM customer;
+
+-- Q28
+SELECT MAX(purch_amt)
+FROM orders;
+
+-- Q29
+SELECT MIN(purch_amt)
+FROM orders;
+
+-- Q30
+SELECT customer_id, MAX(purch_amt) AS highest_purchase_amount
+FROM orders 
+GROUP BY customer_id
+ORDER BY MAX(purch_amt) DESC;
+
+-- Q31
+SELECT customer_id,ord_date,MAX(purch_amt) AS highest_purchase_amount
+FROM orders 
+GROUP BY customer_id,ord_date 
+HAVING MAX(purch_amt) BETWEEN 2000 AND 6000;
+
+-- Q32
+SELECT ord_date,salesman_id,COUNT(*) AS numberOfSalesman 
+FROM orders 
+GROUP BY ord_date,salesman_id;
+
+-- Q33
+SELECT * 
+FROM orders 
+ORDER BY order_no;
+
+-- Q34
+SELECT * 
+FROM orders 
+ORDER BY ord_date DESC;
+
+-- Q35
+SELECT * 
+FROM orders 
+ORDER BY ord_date, purch_amt DESC;
