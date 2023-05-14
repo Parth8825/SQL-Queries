7e8777723cbdd952bf6bd7a2b0753757260046b9
Parth Darji <parthdarji20@gmail.com>
2023-05-14 18:42:36 -0400

    Update Assignment01SQLQuery.sql
    small changes

--- Assignment01/Assignment01SQLQuery.sql
+++ Assignment01/Assignment01SQLQuery.sql
@@ -13,27 +13,27 @@ UPDATE customer SET country = 'UK' where customer_id = 3008;
 UPDATE customer SET country = 'Germany' where customer_id = 3009;
 
 /* Q7. Write a sql statement to display all the information of all salesman */
-SELECT * FROM salesman;
+SELECT * FROM Inventory.dbo.salesman;
 
 /* Q8. Write a sql statement to display specific columns like name and commission for all the salesmen */
 SELECT name, commission
-FROM salesman;
+FROM Inventory.dbo.salesman;
 
 /* Q9. Write a sql statement to display all the information of customer who lives in USA. */
-SELECT * FROM customer
+SELECT * FROM Inventory.dbo.customer
 WHERE country = 'USA';
 
 /* Q10. Write a sql statement to display all the information of those customers with a grade of 200. */
-SELECT * FROM customer
+SELECT * FROM Inventory.dbo.customer
 WHERE grade = 200;
 
 /* Q11. write a sql statement to display information of salesman whose commission is greater than 0.12 */
-SELECT * FROM salesman
+SELECT * FROM Inventory.dbo.salesman
 WHERE commission > 0.12;
 
 /* Q12. write a sql statement to display order information where purchase amount is below 2000 */
-SELECT * FROM orders
+SELECT * FROM Inventory.dbo.orders
 WHERE purch_amt < 2000;
 
 /* Q13. Update customer table to remove the column called Country. */
-ALTER TABLE customer DROP COLUMN country;
+ALTER TABLE Inventory.dbo.customer DROP COLUMN country;
